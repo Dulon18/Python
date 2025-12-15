8858f0d1ab3b29c31d202e0d5e3f48bcadbdefab
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2025-12-15 20:12:01 +0600

    Add age calculation script in practice1.py

---
+++ Day 1/practice1.py
@@ -0,0 +1,8 @@
+# Simpler version - just basic math
+birth_year = int(input("Enter your birth year: "))
+current_year = 2024
+
+age_in_years = current_year - birth_year
+age_in_days = age_in_years * 365
+
+print(f"You are approximately {age_in_days} days old!")
